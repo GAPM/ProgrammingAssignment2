e7180073e6645feeb2453d27ae022654ceb8b36f
Gerardo A. Pirela M <gpirela@gmail.com>
2016-11-21 02:01:32 -0400

    Add modified cachematrix.R for grading
    Commit changes to cachematrix.R as part of 2nd R Programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
+## makeCacheMatrix creates a special object which can store
+## a matrix and cache its inverse/
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
+## cacheSolve returns the inverse of its parameter x
+## x is a special object which has its inversed cached,
+## unless its contents has changed, in which case, 
+## the new inverse is computed.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
